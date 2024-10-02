8e56c0a18cbf97848259990484194b00272a0167
Rajesh <rj.chaudhary5213@gmail.com>
2024-10-02 14:27:29 +0530

    practice loop

---
+++ Loop.java
@@ -0,0 +1,82 @@
+class Loop {
+    public static void main(String[] args){
+
+        // ==========>>>>>> do while loop
+
+        int number = 5;
+        int multiplier = 1;
+
+        do {
+            System.out.printf("%d * %d = %d \n", number, multiplier, number * multiplier);
+
+            multiplier++;
+        }while(multiplier <= 10);
+
+
+
+
+        // end of do while loop ==========<<<<<< 
+
+
+        // ==========>>>>>> While loop
+        
+
+        // int number = 5;
+        // int multiplier = 1;
+
+        // while (multiplier <= 10){
+        //     System.out.printf("%d * %d = %d \n", number, multiplier, number * multiplier);
+        //     multiplier++;
+        // }
+
+        // end of while loop ==========<<<<<< 
+
+
+
+        // ==========>>>>>> for loop
+
+        // int numbers[] = {1, 2, 3, 4, 5};
+
+        // int sum = 0;
+
+        // for (int number : numbers){
+        //     // System.out.println(number);
+        //     sum+=number;
+        // }
+        // System.out.println(sum);
+
+
+        // for(int number = 1; number <= 50; number++){
+        //     if(number%2==1){
+        //         System.out.println(number);
+        //     }
+        // }
+
+
+        // for(int number = 1; number <= 10; number++){
+        //     for(int multiplier = 1; multiplier <= 10; multiplier++){
+        //         System.out.printf("%d * %d = %d\n", number, multiplier, number * multiplier);
+        //     }
+        //     // System.out.println();
+        // }
+
+        // int number = 5;
+
+        // for(int multiplier = 1; multiplier < 10; multiplier++){
+        //     System.out.printf("%d * %d = %d\n", number, multiplier, number * multiplier);
+        // }
+
+        // int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        // int sum=0;
+
+        // for(int index = 0; index < numbers.length; index++){
+        //     // System.out.println(numbers[index]);
+        //     sum+=numbers[index];
+        //     // System.out.println(sum);
+        // }
+        // System.out.println(sum);
+
+        // end of for loop ==========<<<<<< 
+
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+ // for(int number = 1; number <= 50; number++){
+        //     if(number%2==1){
+        //         System.out.println(number);
+        //     }
+        // }
